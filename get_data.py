11ffe39392136060147dff4f8753743fc43e99f3
Sebastian Øverhaug <Overhaug15@gmail.com>
2019-09-23 17:15:29 +0200

    change to using grequests instead of requests, add asynchronous call for fetching of images

--- get_data.py
+++ get_data.py
@@ -1,34 +1,60 @@
 #!/usr/bin/env python
+# A script to extract data from the Washington Post corpus, as well as images attached to the articles
 
 import json_lines as jl
 import json
-import requests
 import time
+import grequests
 
 
-def main(num_of_articles):
+def main(num_of_articles, batch):
     articles = []
     with open('../HuJuData/data/data/TREC_Washington_Post_collection.v2.jl', 'rb') as f:
-        num = 1
+        article_sublist = []
+        num = 0
         for item in jl.reader(f):
             if item['type'] == "article":
-                if image_exists(item):
-                    get_images(item)
+                img_url = get_images(item)
+                if img_url is not None:
+                    article_sublist.append(
+                        {'id': item['id'], 'url': get_images(item)})  # temp list to make requests easier
                     articles.append(item)
-                    print(num, 'of', num_of_articles)
-                    num += 1
-            if len(articles) == num_of_articles:
-                print('Limit reached')
-                break
+
+                    if len(article_sublist) == batch:
+                        fetcher = ImageFetcher((x['url'] for x in article_sublist),
+                                               (y['id'] for y in article_sublist))
+
+                        print('Fetching images ...')
+                        fetcher.async()
+
+                        if len(fetcher.incomplete) > 0:
+                            articles = remove_failed_requests(fetcher.incomplete, articles)
+
+                        article_sublist = []
+
+                        num += batch
+                        print(num, 'of', num_of_articles)
+                        if len(articles) == num_of_articles:
+                            print('Limit reached')
+                            break
         f.close()
     return articles
 
 
+def remove_failed_requests(failed, article_data):
+    for item in article_data:
+        for content in item['contents']:
+            url = get_images(content)
+            if url in failed:
+                article_data.remove(item)
+    return article_data
+
+
 # Writes the data to a json file
 def write_to_file(articles):
-    with open('../HuJuData/data/data/snippet.json', 'w', encoding='utf-8') as file_handler:
-        for data in articles:
-            file_handler.write(json.dumps(data, indent=4, ensure_ascii=False))
+    with open('../HuJuData/data/data/snippetTest.json', 'w', encoding='utf-8') as file_handler:
+        for item in articles:
+            file_handler.write(json.dumps(item, indent=2, ensure_ascii=False))
 
         print('Wrote to file')
         file_handler.close()
@@ -37,31 +63,39 @@ def write_to_file(articles):
 # Fetches images by URL on the current article.
 def get_images(current_article):
     for content in current_article['contents']:
-        if 'fullcaption' in content:
-            img_data = requests.get(content['imageURL']).content
-            with open('../HuJuData/data/data/images/' + current_article['id'] + '.jpg', 'wb') as handler:
-                handler.write(img_data)
-                print('Successfully downloaded image', current_article['id'])
-                return True
-
-
-# Checks if content-object is of type image, and if it's the main image of the article (by presence of fullcaption)
-def image_exists(current_article):
-    for content in current_article['contents']:
         if content['type'] == 'image':
             if 'fullcaption' in content:
                 if len(content['fullcaption']) > 0:
-                    return True
+                    return str(content['imageURL'])
+
+
+class ImageFetcher:
+    def __init__(self, url, ids):
+        self.ids = [x for x in ids]
+        self.urls = [x for x in url]
+        self.incomplete = []
+
+    def exception(self, request, exception):
+        print("Problem: {}: {}".format(request.url, exception))
+        self.incomplete.append(request.url)
+        self.urls.remove(request.url)
+
+    def async(self):
+        results = grequests.map((grequests.get(u) for u in self.urls), exception_handler=self.exception, size=500)
+        for x in range(len(self.urls)):
+            with open('../HuJuData/data/data/images/' + self.ids[x] + '.jpg', 'wb') as handler:
+                handler.write(results[x].content)
+                print('Successfully downloaded image', self.ids[x])
 
 
 if __name__ == '__main__':
     start = time.time()
 
-    data = main(1000)
+    data = main(num_of_articles=50, batch=10)
     write_to_file(data)
 
     end = time.time()
 
     time = end - start
 
-    print('Process took ~%.3g minutes' % int(time/60))
+    print("Process took ~{:.3g} minutes {}".format(int(time)/60, time))
