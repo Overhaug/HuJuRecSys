441e12d71a2bab3b49e9d238a842e5c716030001
Sebastian Øverhaug <Overhaug15@gmail.com>
2019-09-24 18:46:01 +0200

    throw away object(s) if request for image fails

--- get_data.py
+++ get_data.py
@@ -9,15 +9,15 @@ import grequests
 
 def main(num_of_articles, batch):
     articles = []
-    with open('../HuJuData/data/data/TREC_Washington_Post_collection.v2.jl', 'rb') as f:
+    with open('../HuJuData/data/TREC_Washington_Post_collection.v2.jl', 'rb') as f:
         article_sublist = []
         num = 0
         for item in jl.reader(f):
             if item['type'] == "article":
-                img_url = get_images(item)
+                img_url = get_image_url(item)
                 if img_url is not None:
                     article_sublist.append(
-                        {'id': item['id'], 'url': get_images(item)})  # temp list to make requests easier
+                        {'id': item['id'], 'url': get_image_url(item)})  # temp list to make requests easier
                     articles.append(item)
 
                     if len(article_sublist) == batch:
@@ -25,10 +25,14 @@ def main(num_of_articles, batch):
                                                (y['id'] for y in article_sublist))
 
                         print('Fetching images ...')
-                        fetcher.async()
+                        started = time.time()
+                        fetcher.get()
 
+                        num_of_images = batch
                         if len(fetcher.incomplete) > 0:
                             articles = remove_failed_requests(fetcher.incomplete, articles)
+                            num_of_images -= len(fetcher.incomplete)
+                        print("Fetched {} images in {} seconds".format(num_of_images, time.time() - started))
 
                         article_sublist = []
 
@@ -43,16 +47,16 @@ def main(num_of_articles, batch):
 
 def remove_failed_requests(failed, article_data):
     for item in article_data:
-        for content in item['contents']:
-            url = get_images(content)
-            if url in failed:
-                article_data.remove(item)
+        url = get_image_url(item)
+        if url in failed:
+            article_data.remove(item)
+            print("Removed object corresponding to failed request")
     return article_data
 
 
 # Writes the data to a json file
 def write_to_file(articles):
-    with open('../HuJuData/data/data/snippetTest.json', 'w', encoding='utf-8') as file_handler:
+    with open('../HuJuData/data/snippetTest.json', 'w', encoding='utf-8') as file_handler:
         for item in articles:
             file_handler.write(json.dumps(item, indent=2, ensure_ascii=False))
 
@@ -61,7 +65,7 @@ def write_to_file(articles):
 
 
 # Fetches images by URL on the current article.
-def get_images(current_article):
+def get_image_url(current_article):
     for content in current_article['contents']:
         if content['type'] == 'image':
             if 'fullcaption' in content:
@@ -78,24 +82,34 @@ class ImageFetcher:
     def exception(self, request, exception):
         print("Problem: {}: {}".format(request.url, exception))
         self.incomplete.append(request.url)
-        self.urls.remove(request.url)
+        index = self.urls.index(request.url)
+        print(index)
+        del self.ids[index]
+        del self.urls[index]
 
-    def async(self):
-        results = grequests.map((grequests.get(u) for u in self.urls), exception_handler=self.exception, size=500)
+    def get(self):
+        results = grequests.map((grequests.get(u) for u in self.urls),
+                                exception_handler=self.exception,
+                                size=500,
+                                stream=False)
         for x in range(len(self.urls)):
-            with open('../HuJuData/data/data/images/' + self.ids[x] + '.jpg', 'wb') as handler:
-                handler.write(results[x].content)
-                print('Successfully downloaded image', self.ids[x])
+            with open('../HuJuData/data/images/' + self.ids[x] + '.jpg', 'wb') as handler:
+                if results[x] is not None:
+                    handler.write(results[x].content)
+                    results[x].close()
+                    #print('Successfully downloaded image', self.ids[x])
+                else:
+                    print('Object at index {} failed'.format(x))
 
 
 if __name__ == '__main__':
     start = time.time()
 
-    data = main(num_of_articles=50, batch=10)
+    data = main(num_of_articles=3000, batch=500)
     write_to_file(data)
 
     end = time.time()
 
     time = end - start
 
-    print("Process took ~{:.3g} minutes {}".format(int(time)/60, time))
+    print("Process took ~{:.3g} minutes {}".format(int(time) / 60, time))
