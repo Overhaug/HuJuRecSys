c46b8be8e9672ea5d2381e66830c352f9a6bc07b
Sebastian Øverhaug <Overhaug15@gmail.com>
2019-10-16 22:28:06 +0200

    .

--- get_data.py
+++ get_data.py
@@ -9,49 +9,55 @@ import grequests
 import json_lines as jl
 import pandas as pd
 import pytz
+import re
+import os
 
 
-def main(_max, batch_size):
+def main(_max=0, batch_size=5000, images=False):
     articles = []
     num = 0
-    with open('../HuJuData/data/TREC_Washington_Post_collection.v2.jl', 'rb') as f:
+    with open('../HuJuData/data/corpus/TREC_Washington_Post_collection.v2.jl', 'rb') as f:
         article_sublist = []
         for article in jl.reader(f):
-            if article['type'] == "article":
-                img_url = get_image_url(article)
-                if img_url is not None:
-                    article_sublist.append(
-                        {'id': article['id'], 'url': get_image_url(article)})  # temp list to make requests easier
+            article = convert_unix_to_datetime(article)
+            article = add_image_url(article)
+            article = remove_type_from_object(article)
+            
+            if article is not None:
+                articles.append(article)
+            if len(articles) == batch_size:
+                num_of_items = batch_size
+                if images:
+                    get_images(article_sublist, articles, num_of_items)
+                article_sublist = []
 
-                    article = convert_unix_to_datetime(article)
-                    article = add_image_url(article)
-                    article = remove_type_from_object(article)
-
-                    articles.append(article)
-
-                    if len(article_sublist) == batch_size:
-                        fetcher = ImageFetcher((x['url'] for x in article_sublist),
-                                               (y['id'] for y in article_sublist))
-
-                        print('Fetching images ...')
-                        started = time.time()
-                        fetcher.get()
-
-                        num_of_items = batch_size
-                        if len(fetcher.incomplete) > 0:
-                            articles = remove_failed_objects(fetcher.incomplete, articles)
-                            num_of_items -= len(fetcher.incomplete)
-                        print("Fetched {} images in {} seconds".format(num_of_items, time.time() - started))
-
-                        article_sublist = []
-
-                        num += num_of_items
-                        print(num, 'of', _max)
-                        if _max - len(articles) in range(0, 25):
-                            print('Finished fetching data ...')
-                            break
-        f.close()
+                num += num_of_items
+                if _max is not 0:
+                    print(num, 'of', _max)
+                else:
+                    print('Processed {} articles'.format(num))
+                get_as_csv(get_image_url(articles), path='../HuJuData/data/processed/image_urls.csv', convert=False)
+                #get_as_csv(articles, path='../HuJuData/data/processed/corpus_csv.csv')
+                articles = []
+                if _max - num in range(-25, 25):
+                    break
+    f.close()
     return articles
+
+
+def get_images(article_sublist, articles, num):
+    fetcher = ImageFetcher((x['url'] for x in article_sublist),
+                           (y['id'] for y in article_sublist))
+
+    print('Fetching images ...')
+    started = time.time()
+    fetcher.get()
+
+    if len(fetcher.incomplete) > 0:
+        articles = remove_failed_objects(fetcher.incomplete, articles)
+        num -= len(fetcher.incomplete)
+    print("Fetched {} images in {} seconds".format(num, time.time() - started))
+    return len(articles)
 
 
 # Removes "type" property from object
@@ -62,16 +68,26 @@ def remove_type_from_object(item):
 
 # Adds image url from content array to root level
 def add_image_url(item):
-    url = get_image_url(item)
+    url = get_singular_image_url(item)
     item['image_url'] = url
     return item
 
 
+# Fetches image URL on the current article.
+def get_singular_image_url(current_article):
+    for content in current_article['contents']:
+        if content is not None and content['type'] == 'image':
+            return str(content['imageURL'])
+
+
 # Convert unix dates to datetime string
 def convert_unix_to_datetime(item):
-    item['published_date'] = dt.utcfromtimestamp(item['published_date'] / 1000.0)\
-        .astimezone(pytz.timezone("America/New_York"))\
-        .strftime('%Y-%m-%d')
+    try:
+        item['published_date'] = dt.utcfromtimestamp(item['published_date'] / 1000.0) \
+            .astimezone(pytz.timezone("America/New_York")) \
+            .strftime('%Y-%m-%d')
+    except TypeError:
+        print('Unable to convert {} '.format(item['published_date']))
     return item
 
 
@@ -89,7 +105,7 @@ def remove_failed_objects(failed, article_data):
 # Modifies the file to be json serializable
 def get_as_json(articles):
     articles = from_array_to_string(articles)
-    with open('../HuJuData/data/snippetTest2.json', 'w', encoding='utf-8') as file_handler:
+    with open('../HuJuData/data/processed/snippetTest2.json', 'w', encoding='utf-8') as file_handler:
         file_handler.write('[')
         for item in articles:
             file_handler.write(json.dumps(item, indent=2, ensure_ascii=False))
@@ -101,21 +117,37 @@ def get_as_json(articles):
         file_handler.close()
 
 
-# Fetches image URL on the current article.
-def get_image_url(current_article):
-    for content in current_article['contents']:
-        if content['type'] == 'image':
-            if 'fullcaption' in content:
-                if len(content['fullcaption']) > 0:
-                    return str(content['imageURL'])
+# returns a list of id-imageURL objects
+def get_image_url(article):
+    urls_list = []
+    for index in range(len(article)):
+        urls = {}
+        num = 0
+        for content in article[index]['contents']:
+            if content is not None and content['type'] == 'image':
+                urls['id'] = article[index]['id'] + '-' + str(num)
+                urls['url'] = content['imageURL']
+                urls_list.append(urls)
+                num += 1
+                urls = {}
+        num = 0
+    return urls_list
 
 
 # Writes data as csv
-def get_as_csv(d):
-    f = from_array_to_string(d)
+def get_as_csv(d, path, convert=True):
+    if convert:
+        f = from_array_to_string(d)
+        df = pd.DataFrame(f)
+    else:
+        df = pd.DataFrame(d)
 
-    df = pd.DataFrame(f)
-    df.to_csv('../HuJuData/data/snippetCSV2.csv', sep=',', index=False)
+    if os.path.exists(path):
+        with open(path, 'a', encoding='utf-8') as x:
+            df.to_csv(x, sep=',', index=False, header=False)
+    else:
+        with open(path, 'w', encoding='utf-8') as f:
+            df.to_csv(f, sep=',', index=False, header=True)
 
 
 # Extract all content objects (in a json array) of type paragraph to a single string
@@ -126,13 +158,20 @@ def from_array_to_string(d):
         for k, v in x.items():
             if k == 'contents':
                 for i in v:
-                    for e, h in i.items():
-                        if e == 'subtype' and h == 'paragraph':
-                            content_string += i['content']
+                    if i is not None:
+                        for e, h in i.items():
+                            if e == 'subtype' and h == 'paragraph':
+                                plain_text = html_to_text(i['content'])
+                                content_string += plain_text
                 x.pop(k)
                 x['text'] = content_string
                 content_string = ''
     return d
+
+
+def html_to_text(text):
+    clean = re.compile('<.*?>')
+    return str(re.sub(clean, '', text))
 
 
 # Downloads images from a given list of urls
@@ -156,7 +195,7 @@ class ImageFetcher:
                                 size=50,
                                 stream=True)
         for x in range(len(self.urls)):
-            with open('../HuJuData/data/images/' + self.ids[x] + '.jpg', 'wb') as handler:
+            with open('../HuJuData/data/processed/images/' + self.ids[x] + '.jpg', 'wb') as handler:
                 if results[x] is not None:
                     handler.write(results[x].content)
                     results[x].close()
@@ -167,7 +206,7 @@ class ImageFetcher:
 if __name__ == '__main__':
     start = time.time()
 
-    get_as_csv(main(_max=250, batch_size=50))
+    main(batch_size=30000, _max=0)
 
     end = time.time()
 
